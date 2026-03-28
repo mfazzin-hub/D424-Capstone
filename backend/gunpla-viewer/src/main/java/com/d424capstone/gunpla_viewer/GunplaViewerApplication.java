825d74241f94229df567052ba079d8881302c72e
Matthew Fazzina <mfazzin@wgu.edu>
2026-03-27 20:29:40 -0400

    Add Spring Boot to project

---
+++ backend/gunpla-viewer/src/main/java/com/d424capstone/gunpla_viewer/GunplaViewerApplication.java
@@ -0,0 +1,13 @@
+package com.d424capstone.gunpla_viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunplaViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunplaViewerApplication.class, args);
+	}
+
+}
---
+++ backend/gunpla-viewer/src/test/java/com/d424capstone/gunpla_viewer/GunplaViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d424capstone.gunpla_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GunplaViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
