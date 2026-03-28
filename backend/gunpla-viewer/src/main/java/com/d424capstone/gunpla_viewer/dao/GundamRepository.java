b47c75ba10ecf92a5a80104a616ee01afcf93f02
Matthew Fazzina <mfazzin@wgu.edu>
2026-03-27 21:16:42 -0400

    Added Angular frontend.

--- backend/gunpla-viewer/src/main/java/com/d424capstone/gunpla_viewer/dao/GundamRepository.java
+++ backend/gunpla-viewer/src/main/java/com/d424capstone/gunpla_viewer/dao/GundamRepository.java
@@ -3,7 +3,9 @@ package com.d424capstone.gunpla_viewer.dao;
 import com.d424capstone.gunpla_viewer.entities.Gundam;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "gundam", path = "gundam")
 public interface GundamRepository extends JpaRepository<Gundam, Integer> {
 
