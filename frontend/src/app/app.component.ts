2b006d43d15527f093c2ec534c9c98e69fc4885b
Matthew Fazzina <mfazzin@wgu.edu>
2026-03-28 16:34:16 -0400

    Setting up router outlet.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterModule, RouterOutlet} from '@angular/router';
 import {GundamListComponent} from './components/gundam-list/gundam-list.component';
+import {routes} from './app.routes';
 
 @Component({
   selector: 'app-root',
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import {GundamListComponent} from './components/gundam-list/gundam-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'search/:keyword', component: GundamListComponent},
+  {path: 'gundams', component: GundamListComponent},
+  {path: '', redirectTo: '/gundams', pathMatch: 'full'}
+
+];
