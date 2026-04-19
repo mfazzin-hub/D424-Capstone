9be7e187d2cbdb9cdf862dd5dc50aa29678dfc93
Matthew Fazzina <mfazzin@wgu.edu>
2026-04-19 09:53:07 -0400

    CrossOrigin fix.

--- backend/gunpla-viewer/src/main/java/com/d424capstone/gunpla_viewer/dao/GundamRepository.java
+++ backend/gunpla-viewer/src/main/java/com/d424capstone/gunpla_viewer/dao/GundamRepository.java
@@ -9,7 +9,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(originPatterns = {
+        "http://localhost:4200",
+        "https://*.vercel.app"
+})
 @RepositoryRestResource(collectionResourceRel = "gundams", path = "gundam")
 public interface GundamRepository extends JpaRepository<Gundam, Integer> {
 
